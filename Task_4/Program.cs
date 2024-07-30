a2bf3b247bb62237944ceea50a0afc1459949d51
Alexeeva <is4@mail.ru>
2024-07-30 12:10:46 +0700

    Дополнили решение 4 задачи

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -4,3 +4,22 @@
 // 568 => 5,6,8
 // 8 => 8
 // 9542 => 9,5,4,2
+
+Console.WriteLine("Введите целое число и нажмите Enter:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int a = number % 10;
+// int b = number / 10 % 10;
+// int c = number / 10 / 10 % 10;
+// Console.Write($"{c}, {b}, {a}");
+
+string s = number.ToString();
+int i = 1;
+// Console.WriteLine(s.Length);
+for (int j = 1; j <= s.Length; j++)
+{
+    int a = number / i % 10;
+    Console.Write(a);
+    i=i*10;
+}
+
